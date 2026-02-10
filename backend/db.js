753d8fdd28575738ffbbb95091af7774ca805cde
Ana Barea <ana_luiza_barea_melo@eaportal.or>
2026-02-09 21:06:24 -0300

    alteração de erros no gitignore

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2";
-
-export const connection = mysql.createConnection({
-  host: "benserverplex.ddns.net",
-  user: "alunos",
-  password: "senhaAlunos",
-  database: "web_03mb"
-});
